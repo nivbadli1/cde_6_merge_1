bf67256a6f506ee40a93c40995b0eff96da42f49
niv <nivbdbd@gmail.com>
2024-07-21 17:50:38 +0300

    Add basic ETL pipeline structure

---
+++ etl_pipeline.py
@@ -0,0 +1,18 @@
+# ETL Pipeline
+
+def extract_data():
+    print("Extracting data from source")
+
+def transform_data():
+    print("Transforming data")
+
+def load_data():
+    print("Loading data to destination")
+
+def main():
+    extract_data()
+    transform_data()
+    load_data()
+
+if __name__ == "__main__":
+    main()
